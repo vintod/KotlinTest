23b219f9f5d66b73c185369ac5cd334dc0798525
xiaweizi <1012126908@qq.com>
2017-09-04 00:29:17 +0800

    create links between classes

--- src/com/xiaweizi/kotlin/test1/ClassTest.kt
+++ src/com/xiaweizi/kotlin/test1/ClassTest.kt
@@ -1,5 +1,20 @@
 package com.xiaweizi.kotlin.test1
 
+/**
+ * 类测试
+ * */
 fun main(args: Array<String>) {
-    println("hello world")
+    println("name")
+    var student = Student("xiaweizi", 12)
+    var teacher = Teacher("qijiayan", 35)
+}
+
+
+class Student(name: String, age: Int): Person(name, age)
+class Teacher(name: String, age: Int): Person(name, age)
+
+open class Person(name: String, age: Int) {
+    init {
+        println(this.javaClass.simpleName + "name: $name, age: $age")
+    }
 }
