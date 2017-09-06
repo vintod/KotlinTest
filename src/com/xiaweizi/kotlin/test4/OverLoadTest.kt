217aa6a6d6561ddf43d926a0eaf8fa7b061ce3e2
xiaweizi <1012126908@qq.com>
2017-09-06 21:21:52 +0800

    kotlin 中的方法重载

---
+++ src/com/xiaweizi/kotlin/test4/OverLoadTest.kt
@@ -0,0 +1,26 @@
+package com.xiaweizi.kotlin.test4
+
+/**
+ * project:     KotlinProject
+ * date:        2017/9/6
+ * author:      xiaweizi
+ * e-mail:      1012126908@qq.com
+ * description: 方法重载
+ */
+
+class OverLoadTest {
+//    fun a(anInt: Int = 0) = anInt
+    fun a(anInt: Int = 0, string: String="") = 1
+//    fun a() = 1
+
+    fun c() = 1
+    fun c(anInt: Int) = ""
+}
+
+fun main(args: Array<String>) {
+    val test = OverLoadTest()
+    test.a(1, "")
+    test.a()
+    test.a(anInt = 2)
+    test.a(string = "")
+}
