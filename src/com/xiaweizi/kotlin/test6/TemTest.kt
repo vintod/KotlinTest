f10408fe014ac43a01339816552679516bde100d
xiaweizi <1012126908@qq.com>
2017-09-09 17:21:47 +0800

    update README.md

---
+++ src/com/xiaweizi/kotlin/test6/TemTest.kt
@@ -0,0 +1,14 @@
+package com.xiaweizi.kotlin.test6
+
+/**
+ * project:     KotlinProject
+ * date:        2017/9/9
+ * author:      xiaweizi
+ * e-mail:      1012126908@qq.com
+ * description:
+ */
+
+fun main(args: Array<String>) {
+    val string:String? = null
+    println(string!!.length)
+}
